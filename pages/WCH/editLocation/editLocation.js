2a407478e8e10577594b635db8aa416f2bbce4e9
Krega0129 <1841628911@qq.com>
2021-01-20 14:15:30 +0800

    2021-1-20 新增地址默认状态

--- pages/WCH/editLocation/editLocation.js
+++ pages/WCH/editLocation/editLocation.js
@@ -20,7 +20,9 @@ import {
 Page({
   data: {
     user: {
-      sex: 1
+      sex: 1,
+      detailedAddress: '',
+      isDefault: 0
     },
     changeSchool: false,
     hasChange: false,
@@ -28,7 +30,6 @@ Page({
     locationIndex: null,
     // 选择的学校
     index: null,
-    // schoolList: ['广东工业大学', '清华大学', '北京大学'],
     schoolList: [],
     // 是否是新增地址
     addNewAddress: false
@@ -92,29 +93,45 @@ Page({
     this.data.hasChange = true
     this.data.user.contactPhone = e.detail.value
   },
+  setDefault() {
+    this.data.user.isDefault = !this.data.user.isDefault
+    this.data.hasChange = true
+  },
   saveLocation() {
     if(/^1\d{10}$/.test(this.data.user.contactPhone)) {
       if(this.data.addNewAddress) {
-        addNewAddress({
-          userId: wx.getStorageSync('userId'),
-          campus: this.data.user.campus,
-          contactName: this.data.user.contactName,
-          sex: this.data.user.sex,
-          contactPhone: this.data.user.contactPhone,
-          detailedAddress: this.data.user.detailedAddress
-        }).then(res => {
-          if(res && res.data && res.data.code === H_config.STATECODE_addNewAddress_SUCCESS) {
-            wx.showToast({
-              title: '新增地址成功！',
-              duration: 1000
-            })
-            setTimeout(() => {
-              wx.navigateBack()
-            }, 500)
-          } else {
-            showToast('新增地址失败，请重试！')
-          }
-        })
+        const reg = /^(?:[\u4e00-\u9fa5·0-9]{2,6})$/
+        const reg1 = /^(?:[\u4e00-\u9fa5A-Za-z0-9]{2,10})$/
+        
+        if(this.data.index == null) {
+          showToast('请选择校区')
+        } else if(!reg.test(this.data.user.contactName)) {
+          showToast('姓名格式错误，请输入2-6个中文')
+        } else if(!reg1.test(this.data.user.detailedAddress)) {
+          showToast('详细地址只能是2-10个字符')
+        } else {
+          addNewAddress({
+            userId: wx.getStorageSync('userId'),
+            campus: this.data.user.campus,
+            contactName: this.data.user.contactName,
+            sex: this.data.user.sex,
+            contactPhone: this.data.user.contactPhone,
+            detailedAddress: this.data.user.detailedAddress,
+            isDefault: Number(this.data.user.isDefault)
+          }).then(res => {
+            if(res && res.data && res.data.code === H_config.STATECODE_addNewAddress_SUCCESS) {
+              wx.showToast({
+                title: '新增地址成功！',
+                duration: 1000
+              })
+              setTimeout(() => {
+                wx.navigateBack()
+              }, 500)
+            } else {
+              showToast('新增地址失败，请重试！')
+            }
+          })
+        }
       } else {
         if(this.data.hasChange) {
           updateAddress({
@@ -125,7 +142,8 @@ Page({
             contactPhone: this.data.user.contactPhone,
             sex: this.data.user.sex,
             detailedAddress: this.data.user.detailedAddress,
-            receiveId: this.data.user.receiveId
+            receiveId: this.data.user.receiveId,
+            isDefault: Number(this.data.user.isDefault)
           }).then(res => {
             // 修改成功后
             if(res && res.data && res.data.code === H_config.STATECODE_updateAddress_SUCCESS) {
